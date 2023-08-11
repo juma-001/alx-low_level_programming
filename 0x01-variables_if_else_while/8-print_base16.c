3269c3b9745944b0c11a565bbce29d49eab42473
juma-001 <jumapaul589@hgmail.com>
2023-08-11 13:31:41 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,15 +4,15 @@
 *
 *Return: is zero
 */
-int main(void)  
+int main(void)
 {
 	int i;
 	char j;
+
 	for (i = 0 ; i < 10 ; i++)
-		putchar(i +'0');
-	for (j = 'a' ;j <= 'f' ; j++)		
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	putchar('\n');
 	return (0);
-	
 }
