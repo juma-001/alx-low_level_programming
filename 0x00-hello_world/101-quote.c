292de93cae3184d292940003a7a0d60a95d16e95
juma-001 <jumapaul589@hgmail.com>
2023-08-10 23:49:19 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,7 @@
  * Return: will be 1
  */
 int main(void)
-{	
-	fputs("and that piece of art is useful\" - Dora Korpar, 
-2015-10-19\n", stdout);
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
